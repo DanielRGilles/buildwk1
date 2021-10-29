79292142e032f800a3baacc35e7f9ad00c77588f
DanielRGilles <danryangilles@gmail.com>
2021-10-29 13:19:23 -0700

    added multiple tests to triangulate and verify results on edge cases, negative numbers and zero

--- pow.test.js
+++ pow.test.js
@@ -3,7 +3,19 @@ const exponentFunc = require('./pow');
 describe('test exponent function', () => {
   it('returns the result of the base and exponent of two numbers', () => {
     const actual = exponentFunc(2, 4);
-    expect(exponentFunc(actual)).toEqual(16);
+    expect(actual).toEqual(16);
+  });
+  it('returns the result of the base and exponent of two numbers', () => {
+    const actual = exponentFunc(5, 2);
+    expect(actual).toEqual(25);
+  });
+  it('returns the result of the base and exponent of two numbers where one of them is negative', () => {
+    const actual = exponentFunc(2, -2);
+    expect(actual).toEqual(.25);
+  });
+  it('returns the result of the base and exponent of two numbers where one of the numbers is 0', () => {
+    const actual = exponentFunc(0, 1);
+    expect(actual).toEqual(0);
   });
  
 });
